a1cf7986bafbdc3dc55c7d3a2298cf47a98d5202
Esme Abbot <60450727+esme-abbot@users.noreply.github.com>
2023-03-09 10:31:07 -0500

    Update Rocky Poles

--- Rocky_Balance_Starter_Code_2023/Rocky_Balance_Starter_Code_23/Rocky_Balance_Starter_Code_23.ino
+++ Rocky_Balance_Starter_Code_2023/Rocky_Balance_Starter_Code_23/Rocky_Balance_Starter_Code_23.ino
@@ -17,7 +17,6 @@
 #include <LSM6.h>
 #include "Balance.h"
 
-
 extern int32_t angle_accum;
 extern int32_t speedLeft;
 extern int32_t driveLeft;
@@ -68,7 +67,7 @@ Balboa32U4Buzzer buzzer;
 Balboa32U4ButtonA buttonA;
 
 
-#define FIXED_ANGLE_CORRECTION (0.26)  // ***** Replace the value 0.25 with the value you obtained from the Gyro calibration procedure
+#define FIXED_ANGLE_CORRECTION (0.36)  // ***** Replace the value 0.25 with the value you obtained from the Gyro calibration procedure
 
 
 
@@ -86,11 +85,13 @@ void BalanceRocky()
 
     // **************Enter the control parameters here
     
-  float Kp = 0;
-  float Ki = 0;
+  float Kp = 8000;
+  float Ki = 1500;
   float Ci = 0;   
   float Jp = 0;
   float Ji = 0;
+  float K = 0.0036;
+  float tau = 0.07;
 
 
 
@@ -109,7 +110,7 @@ void BalanceRocky()
    // dist_accum - integral of the distance
 
    // *** enter an equation for v_d in terms of the variables available ****
-    v_d =  // this is the desired velocity from the angle controller 
+    v_d =  Kp*angle_rad + Ki * angle_rad_accum;// this is the desired velocity from the angle controller 
       
 
   // The next two lines implement the feedback controller for the motor. Two separate velocities are calculated. 
@@ -119,8 +120,8 @@ void BalanceRocky()
   // right to left. This helps ensure that the Left and Right motors are balanced
 
   // *** enter equations for input signals for v_c (left and right) in terms of the variables available ****
-    v_c_R = 
-    v_c_L =        
+    v_c_R = v_d * K/tau;
+    v_c_L = v_d * K/tau;       
 
 
 
